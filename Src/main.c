6ce81be66c09e94d26fba4cbf1f412df0ed03fde
ryan3-10 <ryan.coutts98@gmail.com>
2026-04-24 16:45:40 -0600

    undo repeat sequence used for testing

--- Src/main.c
+++ Src/main.c
@@ -18,6 +18,7 @@
 
 #include <game.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
