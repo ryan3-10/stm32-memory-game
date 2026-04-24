f5761dd20a34f518e950df5d7067266c5a842a2d
ryan3-10 <ryan.coutts98@gmail.com>
2026-04-23 21:04:16 -0600

    add clarifying comments

--- Inc/components.h
+++ Inc/components.h
@@ -1,14 +1,17 @@
 #ifndef COMPONENTS_H_
 #define COMPONENTS_H_
 
+// Ports
 #define LIGHT_PORT	(GPIOE)
 #define BUTTON_PORT	(GPIOC)
 
+// Light pins
 #define	RED_LIGHT_PIN 		(0x6u)
 #define	WHITE_LIGHT_PIN 	(0x2u)
 #define BLUE_LIGHT_PIN 		(0xDu)
 #define YELLOW_LIGHT_PIN	(0xFu)
 
+// Button pins
 #define RED_BUTTON_PIN		(0x3u)
 #define WHITE_BUTTON_PIN	(0x4u)
 #define BLUE_BUTTON_PIN 	(0x5u)
