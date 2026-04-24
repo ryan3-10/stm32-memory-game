fa322ec5278f703b9554dce64ba82aeb68fa9cd9
ryan3-10 <ryan.coutts98@gmail.com>
2026-04-23 19:49:12 -0600

    refactor game_init function into game.c

---
+++ Inc/game.c
@@ -0,0 +1,25 @@
+void init_game() {
+	// Enable clock for ports C and E
+	RCC->AHB1ENR |= 1 << 2;
+	RCC->AHB1ENR |= 1 << 4;
+
+	// Set the pins controlling the LEDs to output
+	gpio_init_output(GPIOE, 6); // red
+	gpio_init_output(GPIOE, 2); // white
+	gpio_init_output(GPIOE, 13); // blue
+	gpio_init_output(GPIOE, 15); //yellow
+
+	// Set the pins controlling the buttons to input
+	gpio_init_input(GPIOC, 3); // red
+	gpio_init_input(GPIOC, 4); // white
+	gpio_init_input(GPIOC, 5); // blue
+	gpio_init_input(GPIOC, 6); // yellow
+
+	// Set pull up resistors in pins controlling the buttons
+	gpio_set_pullup(GPIOC, 3);
+	gpio_set_pullup(GPIOC, 4);
+	gpio_set_pullup(GPIOC, 5);
+	gpio_set_pullup(GPIOC, 6);
+}
+
+
