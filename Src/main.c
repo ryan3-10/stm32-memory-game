5d41dee00367548b98a605c8c364d69d86801b1f
ryan3-10 <ryan.coutts98@gmail.com>
2026-05-01 17:48:20 -0600

    remove dupliciate directive

--- Src/main.c
+++ Src/main.c
@@ -19,7 +19,6 @@
 #include <board.h>
 #include <game.h>
 #include <system.h>
-//#include <system.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
