01b0bab86e85dd4ff015f476063826d087082c2f
ryan3-10 <ryan.coutts98@gmail.com>
2026-05-01 16:39:35 -0600

    add systick.h/c files

---
+++ Inc/systick.h
@@ -0,0 +1,8 @@
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+#include <stdint.h>	// for uint32_t
+
+void delay(uint32_t count);
+
+#endif /* SYSTICK_H_ */
---
+++ Src/systick.c
@@ -0,0 +1,27 @@
+#include <stdint.h>	// for uint32_t
+#include <mcu.h>	// for SYSTICK
+#include <systick.h>
+
+// Systick flags
+#define CTRL_ENABLE    (1 << 0)
+#define CTRL_CLKSOURCE (1 << 2)
+#define CTRL_COUNTFLAG (1 << 16)
+
+void delay(uint32_t ms) {
+    // 16000 ticks = 1 ms at 16 MHz
+    SYSTICK->LOAD = 16000 - 1;
+
+    // Clear current value register
+    SYSTICK->VAL = 0;
+
+    // Enable SysTick, use processor clock
+    SYSTICK->CTRL = CTRL_ENABLE | CTRL_CLKSOURCE;
+
+    for (uint32_t i = 0; i < ms; i++) {
+        // Wait until COUNTFLAG is set
+        while (!(SYSTICK->CTRL & CTRL_COUNTFLAG));
+    }
+
+    // Disable SysTick
+    SYSTICK->CTRL = 0;
+}
