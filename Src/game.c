ed0ce5a49b4e3e68a1d05a53e2df7f2c90f1dad2
ryan3-10 <ryan.coutts98@gmail.com>
2026-05-06 18:35:42 -0600

    change game_instance variable to just game

--- Src/game.c
+++ Src/game.c
@@ -10,33 +10,33 @@ typedef struct {
 	LIGHT sequence[15];
 } GAME;
 
-static GAME game_instance;
+static GAME game;
 
 void game_reset() {
-	game_instance.state = GAME_WAIT_START;
-	game_instance.round = 1;
-	game_instance.sequence[0] = rand() % BUTTON_COUNT;
+	game.state = GAME_WAIT_START;
+	game.round = 1;
+	game.sequence[0] = rand() % BUTTON_COUNT;
 }
 
 GAME_STATE game_get_state() {
-	return game_instance.state;
+	return game.state;
 }
 
 void game_set_state(GAME_STATE state) {
-	game_instance.state = state;
+	game.state = state;
 }
 
 LIGHT* game_get_sequence() {
-	return game_instance.sequence;
+	return game.sequence;
 }
 
 uint32_t game_get_round() {
-	return game_instance.round;
+	return game.round;
 }
 
 void game_level_up() {
-	if (++game_instance.round <= 15) {
-		game_instance.sequence[game_instance.round - 1] = rand() % BUTTON_COUNT;
+	if (++game.round <= 15) {
+		game.sequence[game.round - 1] = rand() % BUTTON_COUNT;
 	}
 }
 
